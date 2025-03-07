b1f5b81d94b09aa72562f1fd6bd972fc20853e68
Yenco Lammens <yenco.lammens@student.howest.be>
2025-03-07 14:10:03 +0100

    Beginning to implement SDL keyreading

--- Minigin/InputManager.cpp
+++ Minigin/InputManager.cpp
@@ -1,8 +1,10 @@
 #include <SDL.h>
 #include "InputManager.h"
+#include <iostream>
 
 bool dae::InputManager::ProcessInput()
 {
+	//Responsive
 	SDL_Event e;
 	while (SDL_PollEvent(&e)) {
 		if (e.type == SDL_QUIT) {
@@ -10,13 +12,41 @@ bool dae::InputManager::ProcessInput()
 		}
 		if (e.type == SDL_KEYDOWN) {
 			
+			if (e.key.keysym.sym == SDLK_w || e.key.keysym.sym == SDLK_z)
+			{
+				std::cout << "move up";
+			}
+			if (e.key.keysym.sym == SDLK_a || e.key.keysym.sym == SDLK_q)
+			{
+				std::cout << "move left";
+			}
+			if (e.key.keysym.sym == SDLK_d)
+			{
+				std::cout << "move right";
+			}
+			if (e.key.keysym.sym == SDLK_s)
+			{
+				std::cout << "move down";
+			}
+			
 		}
 		if (e.type == SDL_MOUSEBUTTONDOWN) {
 			
 		}
 		
+		
 		// etc...
 	}
 
 	return true;
+}
+
+dae::GameObjectCommand::GameObjectCommand(GameObject* gameObject)
+	:m_gameObject(gameObject)
+{
+
+}
+
+dae::GameObjectCommand::~GameObjectCommand()
+{
 }
--- Minigin/InputManager.h
+++ Minigin/InputManager.h
@@ -1,12 +1,62 @@
 #pragma once
 #include "Singleton.h"
+#include <iostream>
+#include <SDL.h>
+#include <vector>
+#include "GameObject.h"
 
 namespace dae
 {
 	class InputManager final : public Singleton<InputManager>
 	{
 	public:
+		enum class ButtonState 
+		{
+			RELEASED,
+			PRESSED,
+			HELD
+
+		};
 		bool ProcessInput();
+
+		
+
+	private:
+		
 	};
 
+	class Command
+	{
+	public:
+		virtual ~Command() = default;
+		virtual void Execute() = 0;
+
+	};
+
+
+	class GameObjectCommand : public Command
+	{
+	public:
+		GameObjectCommand(GameObject* gameObject);
+		virtual ~GameObjectCommand();
+
+	protected:
+		GameObject* GetGameObject() const { return m_gameObject; }
+
+	private:
+		GameObject* m_gameObject;
+
+	};
+
+	class Move : public GameObjectCommand 
+	{
+		public: void Execute() override
+		{
+			//GetGameObject()->Move();
+		}
+	};
+		
+	
+
+	
 }
